8ca8e5379a4bd524abcc6c46a6f23709bbded738
JustinMShea <jshea@mail.roosevelt.edu>
2018-10-21 21:27:27 -0500

    phoenixdown description

---
+++ R/phoenixdown.R
@@ -0,0 +1,14 @@
+#' phoenixdown: A package for creating the University of Chicago Graham School
+#' Master of Science in Analytics Capstone project in R Markdown.
+#'
+#' @section capstone_gitbook:
+#' Creates a Capstone project template as a webpage
+#' @section capstone_pdf:
+#' Creates a Capstone project template as a PDF
+#' @section capstone_word:
+#' Creates a Capstone project template a Microsoft Word document
+#' @section capstone_epub:
+#' Creates a Capstone project template as an ebook
+#' @docType package
+#' @name phoenixdown
+NULL
